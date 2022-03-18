b27acb46eb78a950dc04274d66da9363a30d99fe
zengqin20 <zengqin20@163.com>
2022-03-18 17:17:39 +0800

    feat: 新增公共变量

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,12 +1,19 @@
 //声明密钥
 const key = "I5CBZ-Z4TKG-KS6QF-IG4XK-UKEVS-QEFBV";
 
-//请求地址
+//腾讯地图请求地址
 const searchBeside = "https://apis.map.qq.com/ws/place/v1/search";
 const searchKey = "https://apis.map.qq.com/ws/place/v1/suggestion";
 
+//小程序相关
+const appid = "wx3487d71fa4384138";
+const secret = "913fcaf9a8e7bca7abe215e77f07cb18";
+const session = "https://api.weixin.qq.com/sns/jscode2session";
 module.exports = {
   key,
   searchKey,
   searchBeside,
+  appid,
+  secret,
+  session,
 };
