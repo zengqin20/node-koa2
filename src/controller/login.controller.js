6c2b344dd72e0baf7a93b7da02a99044eb426eda
zengqin20 <zengqin20@163.com>
2022-03-23 09:31:49 +0800

    refactor: 重构注册登录逻辑

--- src/controller/login.controller.js
+++ src/controller/login.controller.js
@@ -8,40 +8,53 @@ class loginController {
 
   async getTokenKey(ctx, next) {
     const code = ctx.request.body.code;
+    const type = ctx.request.body?.type;
 
     let res = await getSession(code);
     res = JSON.parse(res);
 
-    //查询数据库是否有openId用户信息
+    //查询不同数据库是否有openId用户信息
+    let dbRes;
+    if (!type) {
+      const userRes = await findInfo("User", { openId: res.openid });
+      const childRes = await findInfo("Child", { openId: res.openid });
 
-    const dbRes = await findInfo("User", { openId: res.openid });
+      dbRes = userRes || childRes;
+    } else {
+      const collectionName = type === "parent" ? "User" : "Child";
+      dbRes = await findInfo(collectionName, { openId: res.openid });
+    }
 
+    //查询数据库结果
     let token = "";
-    let type = "";
+    let userType = "";
 
     //查询结果存在
     if (dbRes) {
       // 生成token;
       token = jwt.sign(res, secretKey);
       //返回用户类型
-      type = dbRes.type;
+      userType = dbRes.type;
     }
 
     ctx.body = {
       token,
       openId: res.openid,
-      type,
+      type: userType,
     };
   }
 
   async handleRegister(ctx, next) {
     const data = ctx.request.body;
+    //分角色存储
+    const collectionName = data.type === "parent" ? "User" : "Child";
 
     //存入数据库 唯一标识openId
-    const res = await createInfo("User", data);
+    const res = await createInfo(collectionName, data);
 
     ctx.body = {
       session: res,
+      sessionType: data.type,
     };
   }
 }
